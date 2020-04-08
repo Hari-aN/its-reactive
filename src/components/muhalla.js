51faf6c0821cd96ebb8148320d5a15e856cd132b
haribhai <bisenharishankar@gmail.com>
2020-04-09 02:15:50 +0530

    Created a copy version of Covid19-Tracker

---
+++ src/components/muhalla.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Muhalla(props) {
+  return (
+    <div className="iframe-container" referrerPolicy="no-referrer" width="100%">
+      <iframe title="muhalla" src="https://covid19.health"></iframe>
+    </div>
+  );
+}
+
+export default Muhalla;
